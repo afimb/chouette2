1b3398c836138190276d52677c2656f5be6024cb
Luc Donnet <luc.donnet@dryade.net>
2012-03-26 11:55:30 +0200

    Add filter by area type for stop_area

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,23 +1,3 @@
 module ApplicationHelper
- 
-  def chouette_line_path(line)
-    line_path(line)
-  end
-
-  def chouette_network_path(line)
-    network_path(line)
-  end
-
-  def chouette_company_path(line)
-    company_path(line)
-  end
-
-  def link_to_order(name, column)
-    css_class = (column == sort_column) ? "current #{sort_direction}" : nil
-    Rails.logger.debug sort_direction.inspect
-    direction = (column == sort_column && sort_direction == "asc") ? "desc" : "asc" 
-    Rails.logger.debug direction.inspect
-    link_to name, { :sort => column, :direction => direction}, {:class => css_class}
-  end
   
 end
