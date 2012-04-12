370f8c62e32c3a94be84834b9a8c3b7959c27d3e
Michel Etienne <michel.etienne@dryade.net>
2012-04-12 16:19:52 +0200

    add timetables

--- spec/factories/chouette_companies.rb
+++ spec/factories/chouette_companies.rb
@@ -1,4 +1,6 @@
 Factory.define :company, :class => "Chouette::Company" do |company|
   company.sequence(:name) { |n| "Company #{n}" }
   company.sequence(:objectid) { |n| "test:Company:#{n}" }
+  company.sequence(:registration_number) { |n| "test-#{n}" }
+
 end
