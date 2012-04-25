c3f4535302656bd54ad58e05356a5793ef117f46
Marc Florisson <marc.florisson@dryade.net>
2012-04-25 15:30:59 +0200

    remove useless facotries and use chouette-ninoxe one's

--- spec/factories.rb
+++ spec/factories.rb
@@ -1,48 +1,4 @@
 FactoryGirl.define do
-  factory :company, :class => "Chouette::Company" do |company|
-    company.sequence(:name) { |n| "Company #{n}" }
-    company.sequence(:objectid) { |n| "test:Company:#{n}" }
-    company.sequence(:registration_number) { |n| "test-#{n}" }
-
-  end
-
-  factory :line, :class => "Chouette::Line" do |line|
-    line.sequence(:name) { |n| "Line #{n}" }
-    line.sequence(:objectid) { |n| "test:Line:#{n}" }
-    line.sequence(:transport_mode_name) { |n| "Bus" }
-
-    line.association :network, :factory => :network
-    line.association :company, :factory => :company
-
-    line.sequence(:registration_number) { |n| "test-#{n}" }
-  end
-
-  factory :route, :class => "Chouette::Route" do |route|
-    route.sequence(:name) { |n| "Route #{n}" }
-    route.sequence(:objectid) { |n| "test:Route:#{n}" }
-
-    route.sequence(:published_name) { |n| "Long route #{n}" }
-    route.sequence(:number) { |n| "#{n}" }
-    route.sequence(:wayback_code) { |n| Chouette::Wayback.new( n % 2) }
-    route.sequence(:direction_code) { |n| Chouette::Direction.new( n % 12) }
-
-    route.association :line, :factory => :line
-  end
-
-  factory :network, :class => "Chouette::Network" do |network|
-    network.sequence(:name) { |n| "Network #{n}" }
-    network.sequence(:objectid) { |n| "test:GroupOfLine:#{n}" }
-    network.sequence(:registration_number) { |n| "test-#{n}" }
-  end
-
-  factory :stop_area, :class => "Chouette::StopArea" do |stop_area|
-    stop_area.sequence(:name) { |n| "StopArea #{n}" }
-    stop_area.latitude 10
-    stop_area.longitude 10    
-    stop_area.areatype "CommercialStopPoint"
-    stop_area.sequence(:registration_number) { |n| "test-#{n}" }
-    stop_area.sequence(:objectid) { |n| "test:StopArea:#{n}" }
-  end
 
   factory :referential do |f|
     f.sequence(:name) { |n| "Test #{n}" }
