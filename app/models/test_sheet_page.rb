d44b24c660c528f199f9b9e596b6db8225f8208b
Michel Etienne <metienne@cityway.fr>
2012-07-05 17:26:40 +0200

    workaround for textile special chars

--- app/models/test_sheet_page.rb
+++ app/models/test_sheet_page.rb
@@ -17,6 +17,28 @@ class TestSheetPage
 
   def load
     self.content = File.read(filename)
+    # workaround for special chars
+    self.content = self.content.gsub('é','&eacute;')
+    self.content = self.content.gsub('è','&egrave;')
+    self.content = self.content.gsub('à','&agrave;')
+    self.content = self.content.gsub('ù','&ugrave;')
+    self.content = self.content.gsub('É','&Eacute;')
+    self.content = self.content.gsub('È','&Egrave;')
+    self.content = self.content.gsub('Ê','&Ecirc;')
+    self.content = self.content.gsub('À','&Agrave;')
+    self.content = self.content.gsub('Ù','&Ugrave;')
+    self.content = self.content.gsub('â','&acirc;')
+    self.content = self.content.gsub('ê','&ecirc;')
+    self.content = self.content.gsub('ô','&ocirc;')
+    self.content = self.content.gsub('î','&icirc;')
+    self.content = self.content.gsub('û','&ucirc;')
+    self.content = self.content.gsub('ë','&eumlc;')
+    self.content = self.content.gsub('ï','&iuml;')
+    self.content = self.content.gsub('ç','&ccedil;')
+    self.content = self.content.gsub('oe','&oelig;')
+    self.content = self.content.gsub('<<','&laquo;')
+    self.content = self.content.gsub('>>','&raquo;')
+    self.content = self.content.gsub('°','&ordm;')
   end
 
   def method_missing(method, *arguments)
