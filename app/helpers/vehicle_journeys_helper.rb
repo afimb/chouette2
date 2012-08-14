14b075295d9dde393a5088df0c3d7a0dec239022
Marc Florisson <marc.florisson@dryade.net>
2012-08-14 17:49:33 +0200

    fix vehicle#show title when not having any stop or any times

--- app/helpers/vehicle_journeys_helper.rb
+++ app/helpers/vehicle_journeys_helper.rb
@@ -28,18 +28,18 @@ module VehicleJourneysHelper
     l(first_vjas.departure_time, :format => :hour).gsub( /  /, ' ') 
   end
   def vehicle_title( vehicle)
-    return t('vehicle_journeys.vehicle_journey.title_stopless') if vehicle.vehicle_journey_at_stops.empty?
+    return t('vehicle_journeys.vehicle_journey.title_stopless', :name => vehicle_name( vehicle)) if vehicle.vehicle_journey_at_stops.empty?
     first_vjas = vehicle.vehicle_journey_at_stops.first
     t('vehicle_journeys.vehicle_journey.title', 
-          :name => vehicle.published_journey_name,
+          :name => vehicle_name( vehicle),
           :stop => first_vjas.stop_point.stop_area.name,
           :time => vehicle_departure(vehicle))
   end
   def edit_vehicle_title( vehicle)
-    return t('vehicle_journeys.edit.title_stopless') if vehicle.vehicle_journey_at_stops.empty?
+    return t('vehicle_journeys.edit.title_stopless', :name => vehicle_name( vehicle)) if vehicle.vehicle_journey_at_stops.empty?
     first_vjas = vehicle.vehicle_journey_at_stops.first
     t('vehicle_journeys.edit.title', 
-          :name => vehicle.published_journey_name,
+          :name => vehicle_name( vehicle),
           :stop => first_vjas.stop_point.stop_area.name,
           :time => vehicle_departure(vehicle))
   end
