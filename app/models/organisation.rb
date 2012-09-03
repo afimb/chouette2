b7e1317561a216b2af63b65d756df629cf17e71a
Michel Etienne <metienne@cityway.fr>
2012-09-03 16:31:01 +0200

    no duplicate name on organisation

--- app/models/organisation.rb
+++ app/models/organisation.rb
@@ -2,4 +2,7 @@
 class Organisation < ActiveRecord::Base
   has_many :users, :dependent => :destroy
   has_many :referentials, :dependent => :destroy
+  
+  validates_presence_of :name
+  validates_uniqueness_of :name
 end
