09419f7fbe6be35abba2b638431e41480e8cf764
Luc Donnet <luc.donnet@dryade.net>
2012-10-11 09:39:10 +0200

    Add year to view for spec

--- spec/views/time_tables/show.html.erb_spec.rb
+++ spec/views/time_tables/show.html.erb_spec.rb
@@ -4,6 +4,7 @@ describe "/time_tables/show" do
   
   assign_referential
   let!(:time_table) { assign(:time_table, create(:time_table)) }
+  let!(:year) { assign(:year, Date.today.cwyear) }
 
   it "should render h2 with the time_table comment" do
     render
