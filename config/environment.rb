e24453fbdc2a9a92b3cce8000550977b0d1e7a59
Michel Etienne <metienne@cityway.fr>
2013-05-28 12:14:35 +0200

    add version for display

--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,9 @@
 # Load the rails application
 require File.expand_path('../application', __FILE__)
 
+
 # Initialize the rails application
 ChouetteIhm::Application.initialize!
+
+# Fix version
+APP_VERSION = '2.0.3'
