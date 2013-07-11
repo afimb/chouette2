030be8df5b402ed7dc4523d22447233a7b6d59da
Marc Florisson <mflorisson@gmail.com>
2013-07-11 11:02:00 +0200

    fix locale expression

--- spec/exporters/chouette/kml/exporter_spec.rb
+++ spec/exporters/chouette/kml/exporter_spec.rb
@@ -12,11 +12,17 @@ describe Chouette::Kml::Exporter do
 
   subject { Chouette::Kml::Exporter.new(first_referential) }
 
-  let(:zip_file_path) { "#{Rails.root}/tmp/exports/test.zip" }
+  let(:tmp_path) { File.join( Rails.root, "tmp")}
+  let(:exports_path) { File.join( tmp_path, "exports")}
+  let(:zip_file_path) { File.join( exports_path, "test.zip")}
   let!(:line) { Factory(:line_with_stop_areas_having_parent) }
   let!(:line2) { Factory(:line_with_stop_areas_having_parent) }
 
   describe "#export" do    
+    before(:each) do 
+      Dir.mkdir( tmp_path) unless File.directory?( tmp_path)
+      Dir.mkdir( exports_path) unless File.directory?( exports_path)
+    end
 
     it "should return a zip file with nothing inside with no objects in arguments" do
       subject.export(zip_file_path, {:export_id => 1, :o => "line"} )                   
