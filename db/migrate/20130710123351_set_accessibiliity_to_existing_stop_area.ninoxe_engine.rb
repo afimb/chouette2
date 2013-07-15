c4b365a555e4e5243ef2f3594e2b5e2fc02822a8
Marc Florisson <mflorisson@gmail.com>
2013-07-15 09:00:42 +0200

    refactor set_accessibiliity migration for performance purpose

--- db/migrate/20130710123351_set_accessibiliity_to_existing_stop_area.ninoxe_engine.rb
+++ db/migrate/20130710123351_set_accessibiliity_to_existing_stop_area.ninoxe_engine.rb
@@ -1,15 +1,10 @@
 # This migration comes from ninoxe_engine (originally 20130710122648)
 class SetAccessibiliityToExistingStopArea < ActiveRecord::Migration
   def up
-    Chouette::StopArea.all.each do |s|
-      if s.mobility_restricted_suitability.nil?
-        s.mobility_restricted_suitability = false
-        s.stairs_availability = false
-        s.lift_availability = false
-        s.int_user_needs = 0
-        s.save
-      end
-    end
+    Chouette::StopArea.update_all( :mobility_restricted_suitability => false,
+                                   :stairs_availability => false,
+                                   :lift_availability => false,
+                                   :int_user_needs => 0)
   end
 
   def down
