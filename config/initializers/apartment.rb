40e33f9d80a9444ba144a830a46b11814c089aea
Michel Etienne <metienne@cityway.fr>
2013-10-17 12:24:19 +0200

    remove FileValidation from apartment scope

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -1,6 +1,6 @@
 Apartment.configure do |config|
   # set your options (described below) here
-  config.excluded_models = ["Referential", "Organisation", "User", "Import", "ImportLogMessage", "Export", "ExportLogMessage", "Delayed::Backend::ActiveRecord::Job", "Api::V1::ApiKey"]        # these models will not be multi-tenanted, but remain in the global (public) namespace
+  config.excluded_models = ["Referential", "Organisation", "User", "Import", "ImportLogMessage", "Export", "ExportLogMessage","FileValidation", "FileValidationLogMessage", "Delayed::Backend::ActiveRecord::Job", "Api::V1::ApiKey"]        # these models will not be multi-tenanted, but remain in the global (public) namespace
 
   # Dynamically get database names to migrate
   config.database_names = lambda{ Referential.pluck(:slug) }
