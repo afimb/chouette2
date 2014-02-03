a96a2e1a2fbd09285a0315fb289fd847cf3e0248
Marc Florisson <mflorisson@gmail.com>
2014-02-03 16:03:06 +0100

    display line error and cct link on import#show

--- app/helpers/compliance_check_tasks_helper.rb
+++ app/helpers/compliance_check_tasks_helper.rb
@@ -2,4 +2,11 @@ module ComplianceCheckTasksHelper
 
   include TypeIdsModelsHelper
 
+  def button_link_class( compliance_check_task )
+    if compliance_check_task.any_error_severity_failure? || compliance_check_task.status == "failed"
+      "btn-danger"
+    else
+      "btn-default"
+    end
+  end
 end
