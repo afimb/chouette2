8157af57a17b75eb4e710de940b8fe15244bf561
Michel Etienne <metienne@cityway.fr>
2014-08-13 11:16:54 +0200

    use font awesome icon for duplicate and combine actions

--- app/controllers/time_table_combinations_controller.rb
+++ app/controllers/time_table_combinations_controller.rb
@@ -8,13 +8,13 @@ class TimeTableCombinationsController < ChouetteController
   def create
     combination = TimeTableCombination.new( params[:time_table_combination].merge( :source_id => parent.id))
     if combination.invalid?
-      flash[:alert] = combination.errors.full_messages.join("<br/>")
+      flash[:error] = combination.errors.full_messages.join("<br/>")
     else
       begin
         combination.combine
         flash[:notice] = t('time_table_combinations.success')
       rescue
-        flash[:alert] = t('time_table_combinations.failure')
+        flash[:error] = t('time_table_combinations.failure')
       end
     end
     redirect_to referential_time_table_path(@referential, @time_table)
