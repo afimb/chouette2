6a60bd6dd570c62956929aa5f51abb974e190260
Luc Donnet <ldonnet@cityway.fr>
2014-08-20 16:32:46 +0200

    Quick fix vehicle journey at stops export when no times for a stop_area, need to add spec

--- app/models/vehicle_journey_export.rb
+++ app/models/vehicle_journey_export.rb
@@ -15,13 +15,19 @@ class VehicleJourneyExport
   def persisted?
     false
   end
+
+  def vehicle_journey_at_stops_matrix
+    (vehicle_journeys.collect{ |vj| vj.vehicle_journey_at_stops.collect(&:departure_time).collect{|time| time.strftime("%H:%M")} }).transpose
+  end
   
   def to_csv(options = {})
-    CSV.generate(options) do |csv|      
-      vehicle_journey_at_stops_matrix = (vehicle_journeys.collect{ |vj| vj.vehicle_journey_at_stops.collect(&:departure_time).collect{|time| time.strftime("%H:%M")} }).transpose
+    CSV.generate(options) do |csv|            
       csv << column_names
       route.stop_points.each_with_index do |stop_point, index|
-        csv << [stop_point.id, stop_point.stop_area.name] + vehicle_journey_at_stops_matrix[index]
+        puts vehicle_journey_at_stops_matrix[index].inspect
+        
+        times = vehicle_journey_at_stops_matrix[index].present? ? vehicle_journey_at_stops_matrix[index] : []
+        csv << [stop_point.id, stop_point.stop_area.name] + times
       end
     end
   end
