b827bc2007a820341314da284a83a431803a573f
Michel Etienne <metienne@cityway.fr>
2014-08-22 10:57:46 +0200

    clean correctly modal combine form

--- app/controllers/time_table_combinations_controller.rb
+++ app/controllers/time_table_combinations_controller.rb
@@ -1,24 +1,39 @@
 class TimeTableCombinationsController < ChouetteController
-  respond_to :js, :only => [:create]
+  respond_to :js, :only => [:new,:create]
 
   belongs_to :referential do
     belongs_to :time_table, :parent_class => Chouette::TimeTable 
   end
+  after_filter :clean_flash
+
+  def clean_flash
+    # only run this in case it's an Ajax request.
+    return unless request.xhr?
+    flash.discard
+  end
+
+  def new
+    @time_table_combination = TimeTableCombination.new(:source_id => parent.id)
+    render :action => :new
+  end
+
 
   def create
     @time_table_combination = TimeTableCombination.new( params[:time_table_combination].merge( :source_id => parent.id))
-    #@time_table = parent
     @year = params[:year] ? params[:year].to_i : Date.today.cwyear
     if @time_table_combination.valid?
       begin
         @time_table = @time_table_combination.combine
         flash[:notice] = t('time_table_combinations.success')
         render "create_success"
-      rescue Exception=> e
-        Rails.logger.error "Error: #{e}"
+        rescue => e
+          Rails.logger.error( "TimeTableCombination error, @time_table_combination=#{@time_table_combination.inspect}")
+          Rails.logger.error( e.inspect)
         flash[:error] = t('time_table_combinations.failure')
         render "create_failure"
       end
+    else 
+      render "create_failure"
     end
 
   end
