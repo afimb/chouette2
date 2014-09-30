4faeb448b3e570d69a516dce70c1d098f973892d
Michel Etienne <metienne@cityway.fr>
2014-09-30 09:45:13 +0200

    set durations to 0 when nil

--- app/inputs/extended_time_picker_input.rb
+++ app/inputs/extended_time_picker_input.rb
@@ -3,9 +3,10 @@ class ExtendedTimePickerInput < Formtastic::Inputs::TimePickerInput
   def value
     return options[:input_html][:value] if options[:input_html] && options[:input_html].key?(:value)
     val = object.send(method)
+    puts val.to_s
     return "00:00:00" if val.is_a?(Date)
     return val.strftime("%H:%M:%S") if val.is_a?(Time)
-    return val if val.nil?
+    return "00:00:00" if val.nil?
     val.to_s
   end     
   
