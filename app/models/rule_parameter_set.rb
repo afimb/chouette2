a1a72d06a84d714890e7892d3bb231220d4a8173
Michel Etienne <metienne@cityway.fr>
2014-10-16 13:34:55 +0200

    new compilance tests, Mantis 29182,29183,29184

--- app/models/rule_parameter_set.rb
+++ app/models/rule_parameter_set.rb
@@ -9,13 +9,14 @@ class RuleParameterSet < ActiveRecord::Base
   attr_accessible :name, :referential_id
 
   def self.mode_attribute_prefixes
-    %w( inter_stop_area_distance_min inter_stop_area_distance_max speed_max speed_min inter_stop_duration_variation_max)
+    %w( allowed_transport inter_stop_area_distance_min inter_stop_area_distance_max speed_max speed_min inter_stop_duration_variation_max)
   end
   def self.general_attributes
     %w( inter_stop_area_distance_min parent_stop_area_distance_max stop_areas_area inter_access_point_distance_min
       inter_connection_link_distance_max walk_default_speed_max
       walk_occasional_traveller_speed_max walk_frequent_traveller_speed_max walk_mobility_restricted_traveller_speed_max
-      inter_access_link_distance_max inter_stop_duration_max facility_stop_area_distance_max)
+      inter_access_link_distance_max inter_stop_duration_max facility_stop_area_distance_max 
+      vehicle_journey_number_max vehicle_journey_number_min check_allowed_transport_modes)
   end
   def self.all_modes
     Chouette::TransportMode.all.map { |m| m.to_s}
@@ -75,7 +76,8 @@ class RuleParameterSet < ActiveRecord::Base
       :walk_mobility_restricted_traveller_speed_max => 1,
       :inter_access_link_distance_max => 300,
       :inter_stop_duration_max => 40,
-      :facility_stop_area_distance_max => 300
+      :facility_stop_area_distance_max => 300,
+      :check_allowed_transport_modes => false
     }
     if mode && self.mode_default_params[ mode.to_sym]
       base.merge!( self.mode_default_params[ mode.to_sym])
@@ -85,108 +87,126 @@ class RuleParameterSet < ActiveRecord::Base
   def self.mode_default_params
     {
     :coach => {
+      :allowed_transport_mode_coach => false,
       :inter_stop_area_distance_min_mode_coach => 500,
       :inter_stop_area_distance_max_mode_coach => 10000,
       :speed_max_mode_coach => 90,
       :speed_min_mode_coach => 40,
       :inter_stop_duration_variation_max_mode_coach => 20},
     :air => {
+      :allowed_transport_mode_air => false,
       :inter_stop_area_distance_min_mode_air => 200,
       :inter_stop_area_distance_max_mode_air => 10000,
       :speed_max_mode_air => 800,
       :speed_min_mode_air => 700,
       :inter_stop_duration_variation_max_mode_air => 60},
     :waterborne => {
+      :allowed_transport_mode_waterborne => false,
       :inter_stop_area_distance_min_mode_waterborne => 200,
       :inter_stop_area_distance_max_mode_waterborne => 10000,
       :speed_max_mode_waterborne => 40,
       :speed_min_mode_waterborne => 5,
       :inter_stop_duration_variation_max_mode_waterborne => 60},
     :bus => {
+      :allowed_transport_mode_bus => false,
       :inter_stop_area_distance_min_mode_bus => 100,
       :inter_stop_area_distance_max_mode_bus => 10000,
       :speed_max_mode_bus => 60,
       :speed_min_mode_bus => 10,
       :inter_stop_duration_variation_max_mode_bus => 15},
     :ferry => {
+      :allowed_transport_mode_ferry => false,
       :inter_stop_area_distance_min_mode_ferry => 200,
       :inter_stop_area_distance_max_mode_ferry => 10000,
       :speed_max_mode_ferry => 40,
       :speed_min_mode_ferry => 5,
       :inter_stop_duration_variation_max_mode_ferry => 60},
     :walk => {
+      :allowed_transport_mode_walk => false,
       :inter_stop_area_distance_min_mode_walk => 1,
       :inter_stop_area_distance_max_mode_walk => 10000,
       :speed_max_mode_walk => 6,
       :speed_min_mode_walk => 1,
       :inter_stop_duration_variation_max_mode_walk => 10},
     :metro => {
+      :allowed_transport_mode_metro => false,
       :inter_stop_area_distance_min_mode_metro => 300,
       :inter_stop_area_distance_max_mode_metro => 2000,
       :speed_max_mode_metro => 60,
       :speed_min_mode_metro => 30,
       :inter_stop_duration_variation_max_mode_metro => 30},
     :shuttle => {
+      :allowed_transport_mode_shuttle => false,
       :inter_stop_area_distance_min_mode_shuttle => 500,
       :inter_stop_area_distance_max_mode_shuttle => 10000,
       :speed_max_mode_shuttle => 80,
       :speed_min_mode_shuttle => 20,
       :inter_stop_duration_variation_max_mode_shuttle => 10},
     :rapid_transit => {
+      :allowed_transport_mode_rapid_transit => false,
       :inter_stop_area_distance_min_mode_rapid_transit => 2000,
       :inter_stop_area_distance_max_mode_rapid_transit => 500000,
       :speed_max_mode_rapid_transit => 300,
       :speed_min_mode_rapid_transit => 20,
       :inter_stop_duration_variation_max_mode_rapid_transit => 60},
     :taxi => {
+      :allowed_transport_mode_taxi => false,
       :inter_stop_area_distance_min_mode_taxi => 500,
       :inter_stop_area_distance_max_mode_taxi => 300000,
       :speed_max_mode_taxi => 130,
       :speed_min_mode_taxi => 20,
       :inter_stop_duration_variation_max_mode_taxi => 60},
     :local_train => {
+      :allowed_transport_mode_local_train => false,
       :inter_stop_area_distance_min_mode_local_train => 2000,
       :inter_stop_area_distance_max_mode_local_train => 500000,
       :speed_max_mode_local_train => 300,
       :speed_min_mode_local_train => 20,
       :inter_stop_duration_variation_max_mode_local_train => 60},
     :train => {
+      :allowed_transport_mode_train => false,
       :inter_stop_area_distance_min_mode_train => 2000,
       :inter_stop_area_distance_max_mode_train => 500000,
       :speed_max_mode_train => 300,
       :speed_min_mode_train => 20,
       :inter_stop_duration_variation_max_mode_train => 60},
     :long_distance_train => {
+      :allowed_transport_mode_long_distance_train => false,
       :inter_stop_area_distance_min_mode_long_distance_train => 2000,
       :inter_stop_area_distance_max_mode_long_distance_train => 500000,
       :speed_max_mode_long_distance_train => 300,
       :speed_min_mode_long_distance_train => 20,
       :inter_stop_duration_variation_max_mode_long_distance_train => 60},
     :tramway => {
+      :allowed_transport_mode_tramway => false,
       :inter_stop_area_distance_min_mode_tramway => 300,
       :inter_stop_area_distance_max_mode_tramway => 2000,
       :speed_max_mode_tramway => 50,
       :speed_min_mode_tramway => 20,
       :inter_stop_duration_variation_max_mode_tramway => 30},
     :trolleybus => {
+      :allowed_transport_mode_trolleybus => false,
       :inter_stop_area_distance_min_mode_trolleybus => 300,
       :inter_stop_area_distance_max_mode_trolleybus => 2000,
       :speed_max_mode_trolleybus => 50,
       :speed_min_mode_trolleybus => 20,
       :inter_stop_duration_variation_max_mode_trolleybus => 30},
     :private_vehicle => {
+      :allowed_transport_mode_private_vehicle => false,
       :inter_stop_area_distance_min_mode_private_vehicle => 500,
       :inter_stop_area_distance_max_mode_private_vehicle => 300000,
       :speed_max_mode_private_vehicle => 130,
       :speed_min_mode_private_vehicle => 20,
       :inter_stop_duration_variation_max_mode_private_vehicle => 60},
     :bicycle => {
+      :allowed_transport_mode_bicycle => false,
       :inter_stop_area_distance_min_mode_bicycle => 300,
       :inter_stop_area_distance_max_mode_bicycle => 30000,
       :speed_max_mode_bicycle => 40,
       :speed_min_mode_bicycle => 10,
       :inter_stop_duration_variation_max_mode_bicycle => 10},
     :other => {
+      :allowed_transport_mode_other => false,
       :inter_stop_area_distance_min_mode_other => 300,
       :inter_stop_area_distance_max_mode_other => 30000,
       :speed_max_mode_other => 40,
@@ -208,6 +228,12 @@ class RuleParameterSet < ActiveRecord::Base
         :name => "valeurs par defaut"
       }.merge( mode_attributes))
   end
+  
+  def allowed(mode)
+    return true unless self.check_allowed_transport_modes
+    puts "#{mode} = "+self.send("allowed_transport_mode_#{mode}").to_s
+    return self.send("allowed_transport_mode_#{mode}") == "1"
+  end
 
   all_modes.each do |mode|
     mode_attribute_prefixes.each do |prefix|
@@ -217,7 +243,7 @@ class RuleParameterSet < ActiveRecord::Base
 
   general_attributes.each do |attribute|
     parameter attribute.to_sym
-    unless attribute == "stop_areas_area"
+    unless attribute == "stop_areas_area" || attribute == "check_allowed_transport_modes"
       validates attribute.to_sym, :numericality => true, :allow_nil => true, :allow_blank => true
     end
   end
