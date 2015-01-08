8a4bb1eb66da1140375071ea153b83946b07aa71
Michel Etienne <metienne@cityway.fr>
2015-01-08 17:12:27 +0100

    manage unauthenticated_root

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -5,10 +5,10 @@ module UsersHelper
   end
 
   def user_default_avatar
-    return "#{root_url}#{image_path('icons/user.png')}" if Rails.application.config.relative_url_root.blank?
+    return "#{authenticated_root_url}#{image_path('icons/user.png')}" if Rails.application.config.relative_url_root.blank?
 
     relative_url_root = Rails.application.config.relative_url_root.gsub( /\//, '')
-    "#{root_url}#{image_path('icons/user.png')}".
+    "#{authenticated_root_url}#{image_path('icons/user.png')}".
       sub( Regexp.new("/#{relative_url_root}/#{relative_url_root}/"), "/#{relative_url_root}/").
       sub( Regexp.new("/#{relative_url_root}//#{relative_url_root}/"), "/#{relative_url_root}/")
   end
--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,11 @@ ChouetteIhm::Application.routes.draw do
 
   devise_scope :user do
     authenticated :user do
-      root :to => 'referentials#index'
+      root :to => 'referentials#index', as: :authenticated_root
     end
 
     unauthenticated do
-      root :to => 'devise/sessions#new'
+      root :to => 'devise/sessions#new', as: :unauthenticated_root
     end
   end
 
