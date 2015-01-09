283a9eed064a72c22c381c5025717b592c6cc4fb
Michel Etienne <metienne@cityway.fr>
2015-01-09 07:54:33 +0100

    manage unauthenticated_root

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -6,7 +6,7 @@
 
    def accept_user_creation
      if !Rails.application.config.accept_user_creation
-       redirect_to root_path
+       redirect_to unauthenticated_root_path
        return false
      else
        return true
