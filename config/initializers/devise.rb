4ce59945a30c29a3347c145e57fa496d55de1bfb
Luc Donnet <ldonnet@cityway.fr>
2015-01-16 12:43:50 +0100

    Add spec for devise and upgrade versions of devise views

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -280,7 +280,7 @@ end
 
 Rails.application.config.to_prepare do
   Devise::SessionsController.layout "devise"
-  Devise::RegistrationsController.layout proc{ |controller| ( action_name == "edit") ? "application" : "devise" }
+  Devise::RegistrationsController.layout proc{ |controller| ( action_name == "edit" || action_name == "update") ? "application" : "devise" }
   Devise::ConfirmationsController.layout "devise"
   Devise::UnlocksController.layout "devise"            
   Devise::PasswordsController.layout "devise"        
--- spec/support/helpers/session_helpers.rb
+++ spec/support/helpers/session_helpers.rb
@@ -2,18 +2,23 @@ module Features
   module SessionHelpers
     def sign_up_with(email, password, confirmation)
       visit new_user_session_path
-      fill_in '#registration_new #user_email', with: email
-      fill_in 'Password', with: password
-      fill_in 'Password confirmation', :with => confirmation
-      click_button 'Sign up'
+      within(".registration_new") do
+        fill_in 'user_organisation_attributes_name', with: "Cityway"
+        fill_in 'user_name', with: "User"
+        fill_in 'user_email', with: email
+        fill_in 'user_password', with: password
+        fill_in 'user_password_confirmation', :with => confirmation
+      end
+      click_button 'S\'inscrire'
     end
 
     def signin(email, password)
       visit new_user_session_path
-      # save_and_open_page
-      fill_in '#session_new #user_email', with: email
-      fill_in 'Password', with: password
-      click_button 'Sign in'
+      within(".session_new") do
+        fill_in 'user_email', with: email
+        fill_in 'user_password', with: password       
+      end
+      click_button 'Se connecter'
     end
   end
 end
