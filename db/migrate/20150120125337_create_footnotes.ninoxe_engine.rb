7ff17fdaab62f37357d2c686c62a7b1e2898d847
Marc Florisson <mflorisson@gmail.com>
2015-01-21 14:35:39 +0100

    add footnote missing sources

---
+++ db/migrate/20150120125337_create_footnotes.ninoxe_engine.rb
@@ -0,0 +1,12 @@
+# This migration comes from ninoxe_engine (originally 20150115153453)
+class CreateFootnotes < ActiveRecord::Migration
+  def change
+    create_table :footnotes do |t|
+      t.integer  "line_id", :limit => 8
+      t.string :code
+      t.string :label
+
+      t.timestamps
+    end
+  end
+end
