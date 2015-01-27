3996ab51e41f887d0fcfd40ad18a5ad4ae488fc1
Michel Etienne <metienne@cityway.fr>
2015-01-27 11:15:14 +0100

    add footnotes oo vehiclejourney CSV exports

--- app/models/neptune_export.rb
+++ app/models/neptune_export.rb
@@ -2,18 +2,20 @@ class NeptuneExport < Export
 
   option :start_date
   option :end_date
+  option :extensions
   
   def export_options
     start_date ||= ""
     end_date ||= ""
+    extensions ||= "1"
     if (start_date.empty? && end_date.empty?)
-       super.merge(:format => :neptune).except(:start_date).except(:end_date)
+       super.merge(:format => :neptune, :extensions => extensions ).except(:start_date).except(:end_date)
     elsif start_date.empty?
-       super.merge(:format => :neptune, :end_date => end_date).except(:start_date)
+       super.merge(:format => :neptune, :extensions => extensions , :end_date => end_date).except(:start_date)
     elsif end_date.empty?
-       super.merge(:format => :neptune, :start_date => start_date).except(:end_date)
+       super.merge(:format => :neptune, :extensions => extensions , :start_date => start_date).except(:end_date)
     else  
-       super.merge(:format => :neptune, :start_date => start_date, :end_date => end_date)
+       super.merge(:format => :neptune, :extensions => extensions , :start_date => start_date, :end_date => end_date)
     end
   end
 
