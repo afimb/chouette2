7498c6e18093dc342c0989447f2000d839bf79ea
Luc Donnet <ldonnet@cityway.fr>
2015-02-04 10:36:01 +0100

    Migrate to secrets.yml for Rails4

--- config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-ChouetteIhm::Application.config.secret_token = 'd96f21039d643a92629a6c5c5f9d21864dc05e8ea3dd81295a10ee8ee1138a671bc5693fe618a3af08bb60f7d6494ff650e1b73237460b9213f1333e187853f4'
