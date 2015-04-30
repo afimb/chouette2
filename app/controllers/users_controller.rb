c69b5908707b81744fb028ca6074664b822e6497
Zakaria BOUZIANE <zbouziane@gmail.com>
2015-04-30 11:43:18 +0200

    Fix merge V2.5 pb for UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,18 +1,28 @@
 class UsersController < BreadcrumbController
 
   defaults :resource_class => User
-  respond_to :html, :only => [:show]
-  
+
+  def create
+    @user = current_organisation.users.build(params[:user])
+
+    if @user.valid?
+      @user.invite!
+      respond_with @user, :location => organisation_user_path(@user)
+    else
+      render :action => 'new'
+    end
+  end
+
+  def update
+    update! do |success, failure|
+      success.html { redirect_to organisation_user_path(@user) }
+    end
+  end
+
   def destroy
     destroy! do |success, failure|
       success.html { redirect_to organisation_path }
     end
-  end  
-
-  protected
-
-  def begin_of_association_chain
-    current_organisation
   end
 
 end
