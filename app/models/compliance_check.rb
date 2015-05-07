d6f9c8d86f1f2c7f482a700d446b7a1a63c8e29d
Luc Donnet <ldonnet@cityway.fr>
2015-05-07 16:39:45 +0200

    Begin to refactor compliance check

--- app/models/compliance_check.rb
+++ app/models/compliance_check.rb
@@ -1,49 +1,39 @@
 class ComplianceCheck
-  extend Enumerize
-  extend ActiveModel::Naming
-  extend ActiveModel::Translation
-  include ActiveModel::Model  
-  attr_reader :datas
-  
-  def initialize(response)    
-    @datas = response
-  end
-  
-  def links
-    {}.tap do |links|
-      datas.links.each do |link|
-        links[link["rel"]] = link["href"] 
-      end    
-    end
-  end
+  include JobConcern
 
   def report
-    report_path = links["action_report"]
-    if report_path      
-      response = Ievkit.get(report_path)
-      ComplianceCheckReport.new(response)
-    else
-      nil
+    Rails.cache.fetch("#{cache_key}/action_report", expires_in: cache_expiration) do
+      report_path = links["action_report"]
+      if report_path      
+        response = Ievkit.get(report_path)
+        ComplianceCheckReport.new(response)
+      else
+        nil
+      end
     end
   end
   
   def compliance_check_validation_report
-    report_path = links["validation_report"]
-    if report_path      
-      response = Ievkit.get(report_path)
-      ComplianceCheckResult.new(response)
-    else
-      nil
+    Rails.cache.fetch("#{cache_key}/validation_report", expires_in: cache_expiration) do
+      report_path = links["validation_report"]
+      if report_path      
+        response = Ievkit.get(report_path)
+        ComplianceCheckResult.new(response)
+      else
+        nil
+      end
     end
   end
 
   def rule_parameter_set
-    rule_parameter_set = links["validation_params"]
-    if rule_parameter_set
-      response = Ievkit.get(rule_parameter_set)
-      rule_parameter_set = RuleParameterSet.new.tap { |rps| rps.parameters = response.validation }
-    else
-      nil
+    Rails.cache.fetch("#{cache_key}/validation_params", expires_in: cache_expiration) do
+      rule_parameter_set = links["validation_params"]
+      if rule_parameter_set
+        response = Ievkit.get(rule_parameter_set)
+        rule_parameter_set = RuleParameterSet.new.tap { |rps| rps.parameters = response.validation }
+      else
+        nil
+      end
     end
   end
 
@@ -60,40 +50,8 @@ class ComplianceCheck
     end
   end
 
-  def id
-    datas.id
-  end
-
-  def status
-    datas.status.downcase
-  end
-
   def format
     datas.type
-  end
-
-  def referential_id
-    Referential.where(:slug => referential_name).id
-  end
-  
-  def referential_name
-    datas.referential
-  end
-  
-  def name
-    datas.action_parameters.name
-  end
-  
-  def user_name    
-    datas.action_parameters.user_name
-  end
-
-  def created_at
-    Time.at(datas.created.to_i / 1000) if datas.created
-  end
-
-  def updated_at
-    Time.at(datas.updated.to_i / 1000) if datas.updated
   end
 
 end
