035ad69d0cfc71526ada775ce74e9509a8a2feb4
Luc Donnet <ldonnet@cityway.fr>
2015-05-21 17:03:18 +0200

    Delete unused class ImportType

--- app/models/import_type.rb
+++
@@ -1,6 +0,0 @@
-class ImportType < ActiveEnum::Base
-  value :id => 'Neptune', :name => 'Neptune'
-  value :id => 'Csv', :name => 'Csv'
-  value :id => 'Gtfs', :name => 'GTFS'
-  value :id => 'Netex', :name => 'NeTEx'
-end
