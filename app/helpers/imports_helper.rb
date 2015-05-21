638a5ea5215b66c24133bc43850340a6b0cd8b29
Alban Peignier <alban@tryphon.eu>
2015-05-21 16:59:44 +0200

    Create ImportsHelper#import_attributes_tag and use it in imports#show

--- app/helpers/imports_helper.rb
+++ app/helpers/imports_helper.rb
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 module ImportsHelper
-  
+
   def fields_for_import_task_format(form)
     begin
       render :partial => import_partial_name(form), :locals => { :form => form }
@@ -8,11 +8,11 @@ module ImportsHelper
       ""
     end
   end
-  
+
   def import_partial_name(form)
     "fields_#{form.object.format.underscore}_import"
   end
-  
+
   def compliance_icon( import_task)
     return nil unless import_task.compliance_check_task
     import_task.compliance_check_task.tap do |cct|
@@ -22,6 +22,25 @@ module ImportsHelper
         return 'icons/link_page.png'
       end
     end
-  end    
+  end
+
+  def import_attributes_tag(import)
+    content_tag :div, class: "import-attributes" do
+      [].tap do |parts|
+        if import.format.present?
+          parts << bh_label(t("enumerize.data_format.#{import.format}"))
+        end
+        parts << content_tag(:div, import_save_mode_icon_tag(import), class: "save-mode")
+      end.join.html_safe
+    end
+  end
+
+  def import_save_mode_icon_tag(import)
+    if import.no_save?
+      fa_stacked_icon "database", base: "ban"
+    else
+      fa_icon "database"
+    end
+  end
 
 end
