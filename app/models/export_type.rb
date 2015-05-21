c2c52182928fa0155a7924c0f0023dbbd529e97b
Luc Donnet <ldonnet@cityway.fr>
2015-05-21 16:58:09 +0200

    Delete unused class ExportType

--- app/models/export_type.rb
+++
@@ -1,7 +0,0 @@
-class ExportType < ActiveEnum::Base
-  value :id => 'Neptune', :name => 'Neptune'
-  value :id => 'Csv', :name => 'Csv'
-  value :id => 'Gtfs', :name => 'GTFS'
-  value :id => 'Netex', :name => 'NeTEx'
-  value :id => 'Hub', :name => 'HUB'
-end
