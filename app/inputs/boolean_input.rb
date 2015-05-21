8dc301b503ae261c788f6d0109ed54fc3b71dc58
Alban Peignier <alban@tryphon.eu>
2015-05-21 17:17:21 +0200

    Add customized Formtastic BooleanInput to align label like other inputs

---
+++ app/inputs/boolean_input.rb
@@ -0,0 +1,9 @@
+class BooleanInput < Formtastic::Inputs::BooleanInput
+  def label_with_nested_checkbox
+    builder.label(
+      method,
+      label_text,
+      label_html_options.merge(class: "label")
+    ) << "" << check_box_html
+  end
+end
