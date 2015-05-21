f77f97682897b80b29b290d338d14fec1252f36f
Alban Peignier <alban@tryphon.eu>
2015-05-21 11:53:46 +0200

    Include properly CleanUp error message in i18n message

--- app/controllers/clean_ups_controller.rb
+++ app/controllers/clean_ups_controller.rb
@@ -1,22 +1,22 @@
 class CleanUpsController < ChouetteController
   respond_to :html, :only => [:create]
 
-  belongs_to :referential 
+  belongs_to :referential
 
   def create
-    clean_up = CleanUp.new( params[:clean_up])
+    clean_up = CleanUp.new(params[:clean_up])
+
     if clean_up.invalid?
       flash[:alert] = clean_up.errors.full_messages.join("<br/>")
     else
       begin
         result = clean_up.clean
         flash[:notice] = result.notice.join("<br/>")
-      rescue
-        flash[:alert] = t('clean_ups.failure') + $!
+      rescue => e
+        flash[:alert] = t('clean_ups.failure', error_message: e.to_s)
       end
     end
     redirect_to referential_path(@referential)
-
   end
-  
+
 end
