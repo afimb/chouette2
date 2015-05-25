f69492f08988fe9dc3e89839011e79deb09ed35a
Alban Peignier <alban@tryphon.eu>
2015-05-25 15:33:02 +0200

    Fixes params permission in JourneyPatternsController for stop_point_ids

--- app/controllers/journey_patterns_controller.rb
+++ app/controllers/journey_patterns_controller.rb
@@ -38,7 +38,7 @@ class JourneyPatternsController < ChouetteController
     @vehicle_journey.update_journey_pattern(resource)
     render "vehicle_journeys/select_journey_pattern"
   end
-  
+
   # overwrite inherited resources to use delete instead of destroy
   # foreign keys will propagate deletion)
   def destroy_resource(object)
@@ -50,11 +50,11 @@ class JourneyPatternsController < ChouetteController
     @journey_patterns ||= @q.result(:distinct => true).order(:name)
   end
 
-  
+
   private
-  
+
   def journey_pattern_params
-    params.require(:journey_pattern).permit( :route_id, :objectid, :object_version, :creation_time, :creator_id, :name, :comment, :registration_number, :published_name, :departure_stop_point_id, :arrival_stop_point_id, :stop_point_ids, :stop_points )
+    params.require(:journey_pattern).permit(:route_id, :objectid, :object_version, :creation_time, :creator_id, :name, :comment, :registration_number, :published_name, :departure_stop_point_id, :arrival_stop_point_id, {:stop_point_ids => []})
   end
 
 end
