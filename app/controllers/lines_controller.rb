622dcf03e722524baad7f73806fbad965995b329
Marc Florisson <mflorisson@gmail.com>
2015-06-05 17:20:32 +0200

    fix permit for footnotes

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -8,7 +8,7 @@ class LinesController < ChouetteController
 
   belongs_to :referential
 
-  def index    
+  def index
     index! do |format|
       format.html {
         if collection.out_of_bounds?
@@ -16,7 +16,7 @@ class LinesController < ChouetteController
         end
         build_breadcrumb :index
       }
-    end       
+    end
   end
 
   def show
@@ -28,12 +28,12 @@ class LinesController < ChouetteController
     end
   end
 
-  # overwrite inherited resources to use delete instead of destroy 
+  # overwrite inherited resources to use delete instead of destroy
   # foreign keys will propagate deletion)
   def destroy_resource(object)
     object.delete
   end
-      
+
   def delete_all
     objects =
       get_collection_ivar || set_collection_ivar(end_of_association_chain.where(:id => params[:ids]))
@@ -42,10 +42,10 @@ class LinesController < ChouetteController
   end
 
   def name_filter
-    respond_to do |format|  
-      format.json { render :json => filtered_lines_maps}  
-    end  
-    
+    respond_to do |format|
+      format.json { render :json => filtered_lines_maps}
+    end
+
   end
 
   protected
@@ -55,7 +55,7 @@ class LinesController < ChouetteController
       { :id => line.id, :name => line.published_name }
     end
   end
-  
+
   def filtered_lines
     referential.lines.select{ |t| t.published_name =~ /#{params[:q]}/i  }
   end
@@ -75,7 +75,7 @@ class LinesController < ChouetteController
       params[:q]["group_of_lines_id_eq"] = ""
       params[:q]["group_of_lines_id_blank"] = "1"
     end
-      
+
     @q = referential.lines.search(params[:q])
     @lines ||= @q.result(:distinct => true).order(:number).paginate(:page => params[:page]).includes([:network, :company])
   end
@@ -83,7 +83,7 @@ class LinesController < ChouetteController
   private
 
   def line_params
-    params.require(:line).permit( :transport_mode, :network_id, :company_id, :objectid, :object_version, :creation_time, :creator_id, :name, :number, :published_name, :transport_mode_name, :registration_number, :comment, :mobility_restricted_suitability, :int_user_needs, :flexible_service, :group_of_lines, :group_of_line_ids, :group_of_line_tokens, :url, :color, :text_color, { footnotes_attributes: [ :code, :label, :_destroy ] } )
+    params.require(:line).permit( :transport_mode, :network_id, :company_id, :objectid, :object_version, :creation_time, :creator_id, :name, :number, :published_name, :transport_mode_name, :registration_number, :comment, :mobility_restricted_suitability, :int_user_needs, :flexible_service, :group_of_lines, :group_of_line_ids, :group_of_line_tokens, :url, :color, :text_color, { footnotes_attributes: [ :code, :label, :_destroy, :id ] } )
   end
 
 end
