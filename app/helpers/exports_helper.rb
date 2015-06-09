8bf59df0653ed0cc0ac6ac76ccfb7d99628ebf85
Luc Donnet <ldonnet@cityway.fr>
2015-06-09 10:09:00 +0200

    Add export format text in index and show pages Refs #36451

--- app/helpers/exports_helper.rb
+++ app/helpers/exports_helper.rb
@@ -12,6 +12,16 @@ module ExportsHelper
   def export_partial_name(form)
     "fields_#{form.object.format.underscore}_export"
   end
+
+  def export_attributes_tag(export)
+    content_tag :div, class: "export-attributes" do
+      [].tap do |parts|
+        if export.format.present?
+          parts << bh_label(t("enumerize.data_format.#{export.format}"))
+        end
+      end.join.html_safe
+    end
+  end
   
   def compliance_icon( export_task)
     return nil unless export_task.compliance_check_task
