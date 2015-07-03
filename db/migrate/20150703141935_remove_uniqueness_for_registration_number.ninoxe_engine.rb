b47f4a57b637533d8a4e0645b1303b0d5af50a8e
Marc Florisson <mflorisson@gmail.com>
2015-07-03 16:28:47 +0200

    add migration coming from ninoxe

---
+++ db/migrate/20150703141935_remove_uniqueness_for_registration_number.ninoxe_engine.rb
@@ -0,0 +1,23 @@
+# This migration comes from ninoxe_engine (originally 20150630135517)
+class RemoveUniquenessForRegistrationNumber < ActiveRecord::Migration
+  def up
+    remove_index "lines", name: "lines_registration_number_key" if index_exists?(:lines, :registration_number, name: "lines_registration_number_key")
+    add_index "lines", ["registration_number"], :name => "lines_registration_number_key"
+
+    remove_index "companies", name: "companies_registration_number_key" if index_exists?(:companies, :registration_number, name: "companies_registration_number_key")
+    add_index "companies", ["registration_number"], :name => "companies_registration_number_key"
+
+    remove_index "networks", name: "networks_registration_number_key" if index_exists?(:networks, :registration_number, name: "networks_registration_number_key")
+    add_index "networks", ["registration_number"], :name => "networks_registration_number_key"
+  end
+  def down
+    remove_index "lines", name: "lines_registration_number_key" if index_exists?(:lines, :registration_number, name: "lines_registration_number_key")
+    add_index "lines", ["registration_number"], :name => "lines_registration_number_key", :unique => true
+
+    remove_index "companies", name: "companies_registration_number_key" if index_exists?(:companies, :registration_number, name: "companies_registration_number_key")
+    add_index "companies", ["registration_number"], :name => "companies_registration_number_key", :unique => true
+
+    remove_index "networks", name: "networks_registration_number_key" if index_exists?(:networks, :registration_number, name: "networks_registration_number_key")
+    add_index "networks", ["registration_number"], :name => "networks_registration_number_key", :unique => true
+  end
+end
