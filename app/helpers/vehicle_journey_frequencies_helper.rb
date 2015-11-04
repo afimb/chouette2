e5861132506bcc245c5d7cc34f00979c3932d2ce
Bruno Perles <bruno@atnos.com>
2015-11-04 13:56:57 +0100

    Add a way to show vehicle journey frequency

--- app/helpers/vehicle_journey_frequencies_helper.rb
+++ app/helpers/vehicle_journey_frequencies_helper.rb
@@ -3,7 +3,7 @@ module VehicleJourneyFrequenciesHelper
     base = 100.0 / 2359.0
     left = base * journey_frequency.first_departure_time.strftime("%H%M").to_i
     right = base * journey_frequency.last_departure_time.strftime("%H%M").to_i
-    width = right - left
+    width = (right < left ? 100 - left : right - left)
     [left.round(2), width.round(2)]
   end
 end
