fb245dbf939ce497a8a7355bc994f78164b4204b
Bruno Perles <bruno@atnos.com>
2015-11-12 11:27:31 +0100

    #40511 - Fix spinner icon

--- app/helpers/job_status_icon_helper.rb
+++ app/helpers/job_status_icon_helper.rb
@@ -5,16 +5,16 @@ module JobStatusIconHelper
     name = object.name
     object_name = object.class.model_name.human.capitalize
 
-    title = ""
+    title = ''
     if %w{ aborted canceled }.include?(status)
-      title += "<span class='name aborted'><i class='fa fa-times'></i>"
+      title += '<span class="name aborted"><i class="fa fa-times"></i>'
     elsif %w{ started scheduled }.include?(status)
-      title += "<span class='name processed'><i class='fa fa-spinner fa-spin'></i>"
-    elsif %w{terminated}.include?(status)
-      title += "<span class='name terminated'><i class='fa fa-check'></i>"
+      title += "<span class=\"name processed progress\" title=\"#{I18n.t('job_status.title.processed')}\"><i class=\"fa fa-clock-o\"></i>"
+    elsif %w{ terminated }.include?(status)
+      title += '<span class="name terminated"><i class="fa fa-check"></i>'
     end
 
-    title += "#{object_name} #{truncate(name, :length => 20)}</span>"
+    title += "#{object_name} #{truncate(name, length: 20)}</span>"
 
     title.html_safe
   end
@@ -23,8 +23,7 @@ module JobStatusIconHelper
     name = object.name
     object_name = object.class.model_name.human.capitalize
 
-    title = "#{object_name} #{truncate(name, :length => 20)}"
-    title.html_safe
+    "#{object_name} #{truncate(name, length: 20)}".html_safe
   end
 
 end
