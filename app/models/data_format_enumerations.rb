bc673b59a85c530a13a3366781c1bffb71795e57
Bruno Perles <bruno@atnos.com>
2016-04-13 11:17:48 +0200

    Fix default on DataFormatEnumerations

--- app/models/data_format_enumerations.rb
+++ app/models/data_format_enumerations.rb
@@ -1,8 +1,11 @@
 module DataFormatEnumerations
   extend Enumerize
   extend ActiveModel::Naming
+
+  DEFAULT_FORMATS = %w(neptune gtfs netex hub)
+  DEACTIVATE_FORMATS = ENV['deactivate_formats_referential'].to_s.split(',').map(&:strip)
   
-  enumerize :data_format, in: (%w(neptune netex gtfs hub).select{ |format|
-    !ENV['deactivate_formats_referential'].to_s.split(',').map(&:strip).include?(format)
-  })
+  enumerize :data_format,
+            in: (DEFAULT_FORMATS.select{ |format| !DEACTIVATE_FORMATS.include?(format) }),
+            default: (DEFAULT_FORMATS - DEACTIVATE_FORMATS).first
 end
