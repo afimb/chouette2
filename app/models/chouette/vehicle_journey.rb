3c339546888dab3411f5e3b9d5b4133be6df2a07
Bruno Perles <bruno@atnos.com>
2016-06-21 11:55:57 +0200

    Fix errors msg and colors on input

--- app/models/chouette/vehicle_journey.rb
+++ app/models/chouette/vehicle_journey.rb
@@ -69,7 +69,8 @@ module Chouette
     def increasing_times
       previous = nil
       vehicle_journey_at_stops.select{|vjas| vjas.departure_time && vjas.arrival_time}.each do |vjas|
-        errors.add( :vehicle_journey_at_stops, 'time gap overflow') unless vjas.increasing_times_validate( previous)
+        #errors.add( :vehicle_journey_at_stops, 'time gap overflow') unless
+        vjas.increasing_times_validate( previous)
         previous = vjas
       end
     end
