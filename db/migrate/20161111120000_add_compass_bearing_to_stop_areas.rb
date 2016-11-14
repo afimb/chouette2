f083e795bd78939744455b75a46eb572bd6bda87
Arne Seime <arne.seime@greenbird.com>
2016-11-14 13:37:01 +0100

    Fixed migration script

--- db/migrate/20161111120000_add_compass_bearing_to_stop_areas.rb
+++ db/migrate/20161111120000_add_compass_bearing_to_stop_areas.rb
@@ -1,12 +1,5 @@
 class AddCompassBearingToStopAreas < ActiveRecord::Migration
-  def up
-    add_column :compass_bearing, :integer, null: true, default: null
-  end
-
-  def data
-  end
-
-  def down
-    remove_columns :compass_bearing
+  def change
+    add_column :stop_areas, :compass_bearing, :integer, default: nil, null: true
   end
 end
