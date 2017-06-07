76cca0c74146a215c64c671ceff9cd44e981b3b5
Samuel Wirzén <samuel.wirzen@webstep.no>
2017-06-07 09:33:16 +0200

    NRP-1610 - Removed import/export parameter for valid codespaces

--- app/models/netexprofile_import.rb
+++ app/models/netexprofile_import.rb
@@ -1,6 +1,6 @@
 class NetexprofileImport < ImportTask
 
-  attr_accessor :valid_codespaces
+  attr_accessor
 
   def action_params
     {
@@ -9,8 +9,7 @@ class NetexprofileImport < ImportTask
         "user_name" => user_name,
         "name" => name,
         "organisation_name" => organisation.name,
-        "referential_name" => referential.name,
-        "valid_codespaces" => valid_codespaces
+        "referential_name" => referential.name
       }
     }
   end
