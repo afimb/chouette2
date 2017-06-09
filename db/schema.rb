dc050e832ceb3911604136b5dc1bc49e5ade8481
Morten Friesgaard <friesgaard@gmail.com>
2017-06-09 14:51:01 +0200

    NRP-1610: fixes scheme for codespace.xmlns_url

--- db/schema.rb
+++ db/schema.rb
@@ -77,7 +77,7 @@ ActiveRecord::Schema.define(version: 20170530110237) do
 
   create_table "codespaces", id: :bigserial, force: :cascade do |t|
     t.string   "xmlns",      null: false
-    t.string   "xmlnsurl",   null: false
+    t.string   "xmlns_url",   null: false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
