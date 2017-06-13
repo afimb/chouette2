486786cc6e636e13ec9a67007925a0457e25b2a9
Morten Friesgaard <friesgaard@gmail.com>
2017-06-13 14:28:56 +0200

    NRP-1610: adds test for codespace

---
+++ spec/features/codespaces_spec.rb
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+require 'spec_helper'
+
+describe "codespaces", :type => :feature do
+  login_user
+
+  let!(:codespaces) { [
+      create(:codespace, :xmlns => "ABC", :xmlns_url => "https://www.mycodepace.com/ns/abc"),
+      create(:codespace, :xmlns => "ZXY", :xmlns_url => "https://www.rutebanken.no/ns/zxy")
+  ] }
+  subject { codespaces.first }
+
+  describe "list" do
+    it "display codespaces" do
+      visit referential_codespaces_path(referential)
+      expect(page).to have_content(codespaces.first.xmlns)
+      expect(page).to have_content(codespaces.last.xmlns)
+    end
+
+  end
+
+  describe "show" do
+    it "display codespace" do
+      visit referential_codespaces_path(referential)
+      click_link "#{codespaces.first.xmlns}"
+      expect(page).to have_content(codespaces.first.xmlns)
+    end
+
+  end
+
+  describe "new" do
+    it "creates codespace and return to show" do
+      visit referential_codespaces_path(referential)
+      click_link I18n.t("codespaces.actions.new")
+      fill_in "codespace_xmlns", :with => "DLE"
+      click_button(I18n.t('formtastic.create',model: I18n.t('activerecord.models.codespace.one')))
+      expect(page).to have_content("DLE")
+    end
+  end
+
+  describe "edit and return to show" do
+    it "edit codespace" do
+      visit referential_codespace_path(referential, subject)
+      click_link I18n.t("codespaces.actions.edit")
+      fill_in "codespace_xmlns", :with => "EFR"
+      click_button(I18n.t('formtastic.update',model: I18n.t('activerecord.models.codespace.one')))
+      expect(page).to have_content("EFR")
+    end
+  end
+
+end
--- spec/models/chouette/codespace_spec.rb
+++ spec/models/chouette/codespace_spec.rb
@@ -3,6 +3,7 @@ require 'spec_helper'
 describe Chouette::Codespace do
 
   subject { build(:codespace) }
+  let!(:codespace) { create(:codespace, :xmlns => "ABC", :xmlns_url => "https://www.mycodepace.com/ns/abc") }
 
   it { should validate_presence_of :xmlns }
   it { should validate_presence_of :xmlns_url }
@@ -23,4 +24,14 @@ describe Chouette::Codespace do
     end
   end
 
+  describe "#nullables empty 2" do
+    it "should set null empty nullable attributes" do
+      codespace.xmlns = 'ABC'
+      codespace.xmlns_url = 'https://www.mycodepace.com/ns/abc'
+      codespace.created_at = ''
+      codespace.updated_at = ''
+      expect(codespace.created_at).to be_nil
+      expect(codespace.updated_at).to be_nil
+    end
+  end
 end
