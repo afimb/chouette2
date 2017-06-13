2f489629b8f0635c8e230ffa43ea7d6613c70b77
Morten Friesgaard <friesgaard@gmail.com>
2017-06-13 14:16:17 +0200

    NRP-1610: adds test for codespace controller

--- spec/controllers/codespaces_controller_spec.rb
+++ spec/controllers/codespaces_controller_spec.rb
@@ -3,12 +3,50 @@ require 'spec_helper'
 describe CodespacesController, type: :controller do
   login_user
 
-  describe "GET #index" do
-    it "returns http success" do
-      pending
-      get 'index'
-      expect(response).to be_success
+  let!(:codespace) { create(:codespace, :xmlns => "ABC", :xmlns_url => "https://www.mycodepace.com/ns/abc") }
+
+  it { is_expected.to be_kind_of(ChouetteController) }
+
+  shared_examples_for "codespace and referential linked" do
+    it "assigns codespace as @codespace" do
+      expect(assigns[:codespace]).to eq(codespace)
+    end
+
+    it "assigns referential as @referential" do
+      expect(assigns[:referential]).to eq(referential)
     end
   end
 
+  describe "GET /index" do
+    before(:each) do
+      get :index,
+          :referential_id => referential.id
+    end
+
+    it "assigns referential as @referential" do
+      expect(assigns[:referential]).to eq(referential)
+    end
+  end
+
+  describe "POST /create" do
+    before(:each) do
+      post :create,
+           :referential_id => referential.id,
+           :codespace => { :xmlns => "IKO", :xmlns_url => "http://juuuuhuu.com/ns/kaa"}
+    end
+
+    it "has set values" do
+      expect(assigns[:codespace].xmlns).to eq("IKO")
+      expect(assigns[:codespace].xmlns_url).to eq("http://juuuuhuu.com/ns/kaa")
+    end
+  end
+
+  describe "GET /show" do
+    before(:each) do
+      get :show, :id => codespace.id,
+          :referential_id => referential.id
+    end
+
+    it_behaves_like "codespace and referential linked"
+  end
 end
