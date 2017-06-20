8c4e0702dd963557951f51145323362b6d1ccf92
Morten Friesgaard <friesgaard@gmail.com>
2017-06-20 08:51:57 +0200

    NRP-1612: adds new field to Company, including Organiation Type

--- app/models/chouette/company.rb
+++ app/models/chouette/company.rb
@@ -4,12 +4,17 @@ class Chouette::Company < Chouette::TridentActiveRecord
 
   validates_format_of :registration_number, :with => %r{\A[0-9A-Za-z_-]+\Z}, :allow_nil => true, :allow_blank => true
   validates_presence_of :name
+  validates_presence_of :organisation_type
   validates_format_of :url, :with => %r{\Ahttps?:\/\/([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?\Z}, :allow_nil => true, :allow_blank => true
+  validates_format_of :public_url, :with => %r{\Ahttps?:\/\/([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?\Z}, :allow_nil => true, :allow_blank => true
 
   def self.nullable_attributes
-    [:organizational_unit, :operating_department_name, :code, :phone, :fax, :email, :url, :time_zone]
+    [:organizational_unit, :operating_department_name, :code, :phone, :fax, :email, :url, :time_zone, :organisation_type, :legal_name, :public_phone, :public_email, :public_url]
   end
 
+  def self.operators
+    where(:organisation_type => 'operator')
+  end
 
 end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161011102719) do
+ActiveRecord::Schema.define(version: 20170616094013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -91,6 +91,11 @@ ActiveRecord::Schema.define(version: 20161011102719) do
     t.string   "registration_number"
     t.string   "url"
     t.string   "time_zone"
+    t.string   "organisation_type"
+    t.string   "legal_name"
+    t.string   "public_email"
+    t.string   "public_url"
+    t.string   "public_phone"
   end
 
   add_index "companies", ["objectid"], name: "companies_objectid_key", unique: true, using: :btree
