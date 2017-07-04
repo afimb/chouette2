c650d78fd24f4e4cb03f34b4bcd602bf1c790372
Morten Friesgaard <friesgaard@gmail.com>
2017-07-04 09:22:48 +0200

    NRP-1614: updates reject rules

--- app/models/chouette/destination_display.rb
+++ app/models/chouette/destination_display.rb
@@ -5,12 +5,11 @@ class Chouette::DestinationDisplay < ActiveRecord::Base
   has_many :destination_display_vias
   has_many :vias, :through => :destination_display_vias
   accepts_nested_attributes_for :destination_display_vias, :reject_if => :reject_set_self
-  accepts_nested_attributes_for :vias #, :reject_if => proc {|a| a['name'].blank?}
-
+  accepts_nested_attributes_for :vias, :reject_if => proc {|a| a['front_text'].blank?}
   validates_presence_of :front_text
 
   def reject_set_self(attributes)
-    exists = attributes['destination_display_id'].present?
+    exists = attributes['destination_display_id'].present? && attributes['via_id'].present?
     is_self = attributes['destination_display_id'] == attributes['via_id']
     !exists || is_self # reject empty id or self
   end
