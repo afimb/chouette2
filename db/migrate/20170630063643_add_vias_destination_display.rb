dac5e358a76fedefb0159d8f54395607e7ab690a
Morten Friesgaard <friesgaard@gmail.com>
2017-07-04 09:26:01 +0200

    NRP-1614: updates migration

--- db/migrate/20170630063643_add_vias_destination_display.rb
+++ db/migrate/20170630063643_add_vias_destination_display.rb
@@ -1,11 +1,12 @@
 class AddViasDestinationDisplay < ActiveRecord::Migration
   def change
     create_table :destination_display_via, :id => false do |t|
-      t.integer  :destination_display_id, :limit => 8
-      t.integer  :via_id, :limit => 8
+      t.integer  :destination_display_id, :limit => 8, :null => false
+      t.integer  :via_id, :limit => 8, :null => false
+      t.integer  :position, :limit => 8
+      t.timestamps
     end
 
-    #add_reference :destination_displays, :destination_display, foreign_key: true
-    #add_index :destination_display_via, [:destination_display_id], :name => "index_destination_display_id_on_destination_display_via"
+    add_index :destination_display_via, [:destination_display_id], :name => "index_destination_display_id_on_destination_display_via"
   end
 end
