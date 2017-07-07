f8a0b126d96bc92b882f4eb1ea5419fcb7bb66ff
Arne Seime <arne.seime@greenbird.com>
2017-07-07 17:10:29 +0200

    Add option on netex import to continue parsing even if errors. useful when testing

--- app/models/netexprofile_import.rb
+++ app/models/netexprofile_import.rb
@@ -1,6 +1,6 @@
 class NetexprofileImport < ImportTask
 
-  attr_accessor :parse_site_frames, :validate_against_schema, :validate_against_profile
+  attr_accessor :parse_site_frames, :validate_against_schema, :validate_against_profile, :continue_on_line_errors
 
   def action_params
     {
@@ -10,9 +10,10 @@ class NetexprofileImport < ImportTask
         "name" => name,
         "organisation_name" => organisation.name,
         "referential_name" => referential.name,
-        "parse_site_frames" => parse_site_frames,
-        "validate_against_schema" => validate_against_schema,
-        "validate_against_profile" => validate_against_profile
+        "parse_site_frames" => parse_site_frames.nil? ? false : @parse_site_frames,
+        "validate_against_schema" => validate_against_schema.nil? ? true : @validate_against_schema,
+        "validate_against_profile" => validate_against_profile.nil? ? true : @validate_against_profile,
+        "continue_on_line_errors" => continue_on_line_errors.nil? ? false : @continue_on_line_errors
       }
     }
   end
