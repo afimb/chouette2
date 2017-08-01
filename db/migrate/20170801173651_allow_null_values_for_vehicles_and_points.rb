64ef53d73af52f7911c03b4e12fc6af41e6d8560
Arne Seime <arne.seime@greenbird.com>
2017-08-01 20:03:28 +0200

    Specify nullable allowed for fields

--- db/migrate/20170801173651_allow_null_values_for_vehicles_and_points.rb
+++ db/migrate/20170801173651_allow_null_values_for_vehicles_and_points.rb
@@ -1,8 +1,8 @@
 class AllowNullValuesForVehiclesAndPoints < ActiveRecord::Migration
   def change
-    change_column :interchanges, :from_point, :string
-    change_column :interchanges, :to_point, :string
-    change_column :interchanges, :from_vehicle_journey, :string
-    change_column :interchanges, :to_vehicle_journey, :string
+    change_column :interchanges, :from_point, :string, :null => true
+    change_column :interchanges, :to_point, :string, :null => true
+    change_column :interchanges, :from_vehicle_journey, :string, :null => true
+    change_column :interchanges, :to_vehicle_journey, :string, :null => true
   end
 end
