8133686a9b803d514c3aec68cbc999d94b7778f9
Arne Seime <arne@seimeconsulting.com>
2017-09-14 16:23:48 +0200

    Fix temp table being leftover inside migration

--- db/migrate/20170914062945_convert_vehicle_journey_at_stop_to_neptune_identified_object.rb
+++ db/migrate/20170914062945_convert_vehicle_journey_at_stop_to_neptune_identified_object.rb
@@ -7,6 +7,7 @@ class ConvertVehicleJourneyAtStopToNeptuneIdentifiedObject < ActiveRecord::Migra
     execute <<-SQL
       select prefix into temporary prefix_table_vjas from public.referentials where slug = current_schema();
       update vehicle_journey_at_stops set object_version = 1, objectid = concat((select prefix from prefix_table_vjas limit 1),':TimetabledPassingTime:',id);
+      drop table prefix_table_vjas;
     SQL
     change_column :footnotes, :objectid, :string, null: false
     add_index "vehicle_journey_at_stops", ["objectid"], :name => "vehicle_journey_at_stops_objectid_key", :unique => true
